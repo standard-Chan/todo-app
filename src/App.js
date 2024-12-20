11f9aa1cb86e523e13e806479bcefb2f4383df39
standard-chan <starp321@naver.com>
2024-12-20 11:04:09 +0900

    feat : todoListItem state, props 전달

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
+import { useState } from 'react';
 import './App.css';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 
 function App() {
+  const [todos, setTodos] = useState([
+    {
+      id : 1,
+      text : '할 일 1',
+      checked : true,
+    },
+    {
+      id : 2,
+      text : '리액트',
+      checked : false,
+    },
+    {
+      id : 3,
+      text : '집 알아보기',
+      checked : true,
+    }
+  ]);
   return (
   <div>
     <TodoTemplate>
       <TodoInsert/>
-      <TodoList/>
+      <TodoList todos={todos}/>
     </TodoTemplate>
   </div>);
 }
--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -4,13 +4,15 @@ import {
  } from 'react-icons/md';
 import { FaTrash } from "react-icons/fa";
 import './TodoListItem.scss';
+import cn from 'classnames';
 
-const TodoListItem = () => {
+const TodoListItem = ({todo}) => {
+  const {text, checked} = todo;
   return(
     <div className='TodoListItem'>
-      <div className='checkbox'>
-        <MdCheckBoxOutlineBlank/>
-        <div className='text'>할 일</div>
+      <div className={cn('checkbox', {checked})}>
+        {checked ? <MdCheckBox/> : <MdCheckBoxOutlineBlank/>}
+        <div className='text'>{text}</div>
       </div>
       <div className='remove'>
         <FaTrash/>
