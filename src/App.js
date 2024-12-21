3656c785bdfc8769006f3d980fd9dd7564372c12
standard-chan <starp321@naver.com>
2024-12-21 23:46:33 +0900

    feat : local storage 저장, 삭제, 읽기 구현

--- src/App.js
+++ src/App.js
@@ -1,55 +1,72 @@
-import { useCallback, useReducer, useRef, useState } from 'react';
+import { useCallback, useEffect, useReducer, useRef, useState } from 'react';
 import './App.css';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 import TodoTemplate from './components/TodoTemplate';
 import Timer from './components/timer/Timer';
+import generateUniqueKey from './utils/hash';
 
 function App() {
   const [todos, setTodos] = useState([
     {
-      id : 1,
-      text : '할 일 1',
+      id : "예시",
+      text : "리액트 공부",
+      date : null,
       checked : true,
     },
-    {
-      id : 2,
-      text : '리액트',
-      checked : false,
-    },
   ]);
+  const [keyList, setKeyList] = useState([]);
 
+  // todo timer
   const [timerForTodo, setTimerForTodo] = useState({
     todoId : 0,
     displayed : false,
   })
 
-  // hash 함수 : 현재 날짜와 업무 내용으로 키 생성
-  const generateUniqueKey = async (date, str) => {
-    const input = `${date}-${str}`;
-    const encoder = new TextEncoder();
-    const data = encoder.encode(input);
-    const hashBuffer = await crypto.subtle.digest('SHA-256', data);
-    const hashArray = Array.from(new Uint8Array(hashBuffer));
-    const hashHex = hashArray.map(b => b.toString(16).padStart(2, '0')).join('');
-    return hashHex;
-  };
+  // local storage 값 불러오기
+  useEffect(()=>{
+    const savedKeyList = localStorage.getItem('key');
+    if (savedKeyList) {
+      const keys = JSON.parse(savedKeyList);
+      const storedTodos = keys.map(key => JSON.parse(localStorage.getItem(key)));
+      setTodos(storedTodos);
+      setKeyList(keys);
+    }
+  },[]);
+
+  useEffect(()=> {
+    const savedTodos = keyList.map(key => localStorage.getItem(key));
+    
+  }, [keyList]);
 
   const onInsert = useCallback(
     async text => {
-      const currentDate = new Date().toISOString();
+      const date = new Date();
+      const currentDate = `${date.getFullYear()}.${date.getMonth()+1}.${date.getDate()}`;
       const uniqueKey = await generateUniqueKey(currentDate, text);
       const todo = {
         id : uniqueKey,
         text : text,
+        date : currentDate,
         checked : false,
       };
+      // local storage에 저장
+      console.log(uniqueKey);
+      localStorage.setItem(uniqueKey, JSON.stringify(todo));
+      const newKeyList = [...keyList, uniqueKey]
+      setKeyList(newKeyList);
+      localStorage.setItem('key', JSON.stringify(newKeyList));
       setTodos(todos.concat(todo));
     }, [todos]);
 
   const onRemove = useCallback(
     id => {
-      setTodos(todos.filter(todo => todo.id !== id))
+      const removedKeyList = keyList.filter(key => key !== id )
+      setTodos(todos.filter(todo => todo.id !== id));
+      setKeyList(removedKeyList);
+      // localstorage 지우기
+      localStorage.removeItem(id);
+      localStorage.setItem('key', removedKeyList);
     }, [todos]);
 
   const onToggle = useCallback(id => {
